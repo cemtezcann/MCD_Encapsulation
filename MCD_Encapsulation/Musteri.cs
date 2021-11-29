c2b93c98ae786c814c57092ef616e041272ea0ca
cmtzc <tezcancem92@gmail.com>
2021-11-29 17:30:19 +0300

    Add project files.

---
+++ MCD_Encapsulation/Musteri.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_Encapsulation
+{
+    class Musteri
+    {
+        //Class =>Field
+        int id;
+        public string isim;
+        public string soyisim;
+
+
+        // Classs =>Property
+        public string emailAdres;
+        public string EmailAdres
+        {
+            //get;//Database veya farklı bir veri kaynağı class çağıran programcıya datayı gösterdiğimiz alan
+            //set;//Dış Dünyadan alınan datanın içerisinde private olarak saklanan field içerisine değer atandığı kısım
+
+            set
+            {
+                this.emailAdres = value;
+            }
+
+            get
+            {
+                return this.emailAdres;
+            }
+        }
+    }
+}
---
+++ MCD_Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+            /*Field deger ataması yapılsın ama deger okunabilsin
+             * Field deger ataması yapılsın ama deger okunamasın
+             * field deger ataması yapılsın ama degerin ilk 4hanesi okunsun
+             * field deger atamsı ve okuma işlemleri yapılsın ama bu iki işlemde bizim
+             * belirlediğimiz bir süreçten geçsin veri daha sonra gösterilsin
+             * 
+             */
+
+            // M1.emailAdres = "turanfaik@asdasdas.com";
+
+            //GEnel olarak kapsülleme konusuna giriş yaptık
+            M1.EmailAdres = "turanfaik@asdasdas.com";
+            Console.WriteLine(M1.EmailAdres);
+        }
+    }
+}
